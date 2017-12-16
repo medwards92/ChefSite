d3e835bc7fd3bb529dcf29a62eefefd68870ba78
medwards92 <6705092+medwards92@users.noreply.github.com>
2017-12-16 15:29:19 -0600

    Initial commit
    Adding everything from TortoiseSVN repo.

---
+++ ChefSite/Scripts/Slideshow.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $('.slides .slide:gt(0)').hide();
+    setInterval(function () {
+        $('.slides :first-child')
+            .fadeOut(1000)
+            .next('.slide')
+            .fadeIn(1000)
+            .end()
+            .appendTo('.slides');
+    }, 5000);
+});
---
+++ ChefSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChefSite.Startup))]
+namespace ChefSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
