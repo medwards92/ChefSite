f3fffc7b72e915ec13f361b28fa215fa427c8c41
medwards92 <6705092+medwards92@users.noreply.github.com>
2017-12-25 13:41:19 -0600

    Contact Updates
    Updating Contact page visuals.

--- ChefSite/Models/ContactViewModel.cs
+++ ChefSite/Models/ContactViewModel.cs
@@ -8,20 +8,22 @@ namespace ChefSite.Models
 {
     public class ContactViewModel
     {
-        [Display(Name = "First Name")]
-        public string FirstName { get; set; }
+        [Display(Name = "Name")]
+        public string FullName { get; set; }
 
-        [Display(Name = "Last Name")]
-        public string LastName { get; set; }
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
+
+        [Display(Name = "Email")]
         public string EmailAddress { get; set; }
+
+        [Display(Name = "Event Date")]
         public DateTime EventDate { get; set; }
+
+        [Display(Name = "Type of Event")]
         public int EventType { get; set; }
-        public string BrideFirstName { get; set; }
-        public string BrideLastName { get; set; }
-        public string GroomFirstName { get; set; }
-        public string GroomLastName { get; set; }
-        public string CompanyName { get; set; }
-        public string Description { get; set; }
+
+        [Display(Name = "Comments")]
+        public string Comments { get; set; }
     }
 }
